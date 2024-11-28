840dab7efc3c5ef38dabf0e1845615a68e6136bf
Biopubler <tor.karlsson@skola.taby.se>
2024-11-28 09:05:45 +0100

    Add files via upload

---
+++ Test för biblotek.py
@@ -0,0 +1,90 @@
+import random as rand
+
+Menyval = input
+
+if Menyval == "q":
+     exit()
+    
+
+print("                                     Välkommen till Bibloteket")
+
+print(
+"""
+                                   .--.                   .---.
+                               .---|__|           .-.     |~~~|
+                            .--|===|--|_          |_|     |~~~|--.--.
+                            |  |===|  |'\     .---!~|  .--|   |--|--|
+                            |%%|   |  |.'\    |===| |--|%%|   |  |  |
+                            |%%|   |  |\.'\   |   | |__|  |   |  |  |
+                            |B | I |B | \L \  |=I=|O|T=|E | K |E |T |
+                            |  |   |__|  \.'\ |   |_|__|  |~~~|__|__|
+                            |  |===|--|   \.'\|===|~|--|%%|~~~|--|--|
+                            ^--^---'--^    `-'`---^-^--^--^---'--'--'
+""")
+
+print("""Skriv in vad du vill göra,
+dina alternativ står på menyn:"""  )
+
+
+#Idén här är att få den här listan att fungera med def func senare och lista ut hur jag kan ge dem synergy med varandra.
+Lista_av_böcker = ["Pippi långstrump"]
+
+print(
+        """
+                                          --- MENY ---
+                Välkommen till biblioteks-simulatorn. Välj ett av alternativen nedan:
+            1. Sök efter titel                                  2. Sök efter författare
+            3. Låna bok                                         4. Återlämna bok
+            4. Lägg till ny bok                                 5. Ta bort bok
+            6. Ta bort bok                                      7. Lista alla böcker
+            8. Sortera böcker                                   q. Avsluta
+        ---------------------------------------------------------------------------------------
+
+        """)
+Menyval = input("Menyval --> ") 
+is_running = True
+while is_running:
+
+     if Menyval == "1":
+      titelsökning = input("Sök--> ")
+
+#Det Jag försöker göra här är att låta oss kunna söka efter en bok.
+      
+      
+       
+
+if Menyval == "2":
+     pass
+
+if Menyval == "3":
+   pass
+
+
+elif Menyval == "4":
+     pass
+
+
+elif Menyval == "5":
+     pass
+
+
+elif Menyval == "6":
+   pass
+
+
+elif Menyval == "7":
+   pass
+
+
+elif Menyval == "8":
+  pass
+
+   
+
+
+
+
+
+
+    
+    
